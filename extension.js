f96d4000392b313d8aa33749e63409f48cbce896
oliadler <oliver_elias.adler@fh-bielefeld.de>
2023-07-31 09:21:34 +0200

    Provides the actual functionality at the current cursor position

--- extension.js
+++ extension.js
@@ -11,43 +11,47 @@ const vscode = require('vscode');
 function activate(context) {
 
 	let disposable = vscode.commands.registerCommand('extension.autoVariable', function () {
-		let editor = vscode.window.activeTextEditor;
+		const editor = vscode.window.activeTextEditor;
+		let isHandled = false;
 		
 
 		if (!editor) {
 			return;
 		}
 		
-			let document = editor.document;
+			const document = editor.document;
+		
 			let selection = editor.selection;
 
 			let lineText = document.lineAt(selection.start.line).text;
 
-		
+
 			let colonPosition = lineText.indexOf(":");
 			let semicolonPosition = lineText.indexOf(";");
 
-			
+			if (colonPosition !== -1 && semicolonPosition !== -1) {    
 				let variableType = lineText.substring(colonPosition + 1, semicolonPosition).trim();
 				
 				const alObjectTypes = ["Record", "Page", "Report", "Codeunit", "XMLport", "Query", "Enum", "DotNet"]; // AL Object Types
 			
 				for (let i = 0; i < alObjectTypes.length; i++) {
-					
+					if (isHandled) {break;}
 					if (variableType.startsWith(alObjectTypes[i])) {
 						let typeName = variableType.substring(alObjectTypes.length).trim();
+						let variableName = typeName.replace(/[\s"]/g, ""); // g = global   
 						
-	
 						let startPosition = new vscode.Position(selection.start.line, 0);
 						let endPosition = new vscode.Position(selection.start.line, colonPosition);
 						let range = new vscode.Range(startPosition, endPosition);
 						
 						editor.edit(function (editBuilder) {
-							editBuilder.replace(range, `${typeName}`);
+							editBuilder.replace(range, `${variableName}`);
+							isHandled = true;
 						});
 					}
 
 				}
+			}
 				
 		});
 	
