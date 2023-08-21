636fae6b8148c06d85c02d0df0e3e87a97fcb678
oliadler <oliver_elias.adler@fh-bielefeld.de>
2023-08-21 12:54:29 +0200

    Considers the whole document now and rename its variables.

--- extension.js
+++ extension.js
@@ -10,55 +10,56 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	let disposable = vscode.commands.registerCommand('extension.autoVariable', function () {
-		const editor = vscode.window.activeTextEditor;
-		let isHandled = false;
-		
+let disposable = vscode.commands.registerCommand('extension.autoVariable', function () {
+    const editor = vscode.window.activeTextEditor;
 
-		if (!editor) {
-			return;
-		}
-		
-			const document = editor.document;
-		
-			let selection = editor.selection;
+    if (!editor) {
+        return;
+    }
 
-			let lineText = document.lineAt(selection.start.line).text;
+    const document = editor.document;
+    const documentLineCounter = document.lineCount;
 
+    const workspaceEdit = new vscode.WorkspaceEdit(); 
 
-			let colonPosition = lineText.indexOf(":");
-			let semicolonPosition = lineText.indexOf(";");
+    for (let i = 0; i < documentLineCounter; i++) {
 
-			if (colonPosition !== -1 && semicolonPosition !== -1) {    
-				let variableType = lineText.substring(colonPosition + 1, semicolonPosition).trim();
-				
-				const alObjectTypes = ["Record", "Page", "Report", "Codeunit", "XMLport", "Query", "Enum", "DotNet"]; // AL Object Types
-			
-				for (let i = 0; i < alObjectTypes.length; i++) {
-					if (isHandled) {break;}
-					if (variableType.startsWith(alObjectTypes[i])) {
-						let typeName = variableType.substring(alObjectTypes.length).trim();
-						let variableName = typeName.replace(/[\s"]/g, ""); // g = global   
-						
-						let startPosition = new vscode.Position(selection.start.line, 0);
-						let endPosition = new vscode.Position(selection.start.line, colonPosition);
-						let range = new vscode.Range(startPosition, endPosition);
-						
-						editor.edit(function (editBuilder) {
-							editBuilder.replace(range, `${variableName}`);
-							isHandled = true;
-						});
-					}
+        let lineText = document.lineAt(i).text;
+        if (lineText === '') { continue; }
+        let colonPosition = lineText.indexOf(":");
+        let semicolonPosition = lineText.indexOf(";");
 
+        if (colonPosition !== -1 && semicolonPosition !== -1) {
+            let variableType = lineText.substring(colonPosition + 1, semicolonPosition).trim();
+
+            const alObjectTypes = ["Record", "Page", "Report", "Codeunit", "XMLport", "Query", "Enum", "DotNet"]; // AL Object Types
+
+            for (let j = 0; j < alObjectTypes.length; j++) {
+                if (variableType.startsWith(alObjectTypes[j])) {
+                    let typeName = variableType.substring(alObjectTypes[j].length).trim();
+                    let variableName = typeName.replace(/[\s".-/&]/g, "");
+
+                    let startPosition = new vscode.Position(i, 0);
+                    let endPosition = new vscode.Position(i, colonPosition);
+                    let range = new vscode.Range(startPosition, endPosition);
+
+                    workspaceEdit.replace(document.uri, range, `${variableName}`);
+					//TODO: Rename References (F2)
 				}
-			}
-				
-		});
-	
+            }
+        }
+    }
+
+    vscode.workspace.applyEdit(workspaceEdit).then(success => {
+        if (success) {
+            document.save();
+        } else {
+            vscode.window.showErrorMessage('Fehler');
+        }
+    });
 	context.subscriptions.push(disposable);
+});
 }
-
-
 
 // This method is called when your extension is deactivated
 function deactivate() {}
